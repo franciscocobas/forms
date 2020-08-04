fdcf7c0d8f99f9ec62ea4cb7b7b2f27a35b1a98a
Francisco <fcobas@vairix.com>
2020-08-04 17:08:58 -0300

    Added to redux the user

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import './Home.scss';
+import { useSelector } from 'react-redux';
 
 export default () => {
+  const { user } = useSelector(({ loginReducer }) => {
+    return { user: loginReducer.user }
+  });
+
   return (
     <div className="home-container">
-
+      <p>{user.email}</p>
     </div>
   )
 }
